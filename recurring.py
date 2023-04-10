224611a75d6cd63fc679afbf4f03b26b96edde29
NingXinran <ningxinran@u.yale-nus.edu.sg>
2023-04-10 19:18:45 +0800

    Remove recurring.py

--- recurring.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-requests.get("https://api.telegram.org/bot6014581360:AAFVmqWRiVZWuTkDuMwghwPobVX8uvkwbMk/sendMessage?chat_id=395443238&text=hey%20xinran!%20press%20/start%20to%20add%20your%20expenses%20for%20today:)&parse_mode=markdown")
