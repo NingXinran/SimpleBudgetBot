856419f3af8eb5e6011cb8a92e3d5d51b0a40091
NingXinran <ningxinran@u.yale-nus.edu.sg>
2023-04-11 22:28:31 +0800

    Edit recurring message

--- recurring.py
+++ recurring.py
@@ -7,4 +7,4 @@ load_dotenv()
 CHAT_ID = os.environ.get('CHAT_ID')
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
-requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text=hey%20xinran!%20press%20/start%20to%20add%20your%20expenses%20for%20today:)&parse_mode=markdown")
+requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text=hey%20xinran!%20press%20/add%20to%20add%20your%20expenses%20for%20today:)&parse_mode=markdown")
